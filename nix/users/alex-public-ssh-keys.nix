9877575e85f2721a183551ee985f9af95044157c
alex <alex@nazar.studio>
2026-05-15 17:26:40 +0300

    Add Termius mobile SSH key

--- nix/users/alex-public-ssh-keys.nix
+++ nix/users/alex-public-ssh-keys.nix
@@ -4,4 +4,7 @@
 [
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIApz+YMw3CCaUJJ5JBil/VjOIUR0Oe46XQSuDrou0bkJ alex@yoga"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGAUXo4tVZqoDmGi1M6oTuzcXiSh+icDMnr5/N4fh2yJ alex@nazar"
+
+  # Termius mobile key.
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII0zCGM+Dj9DJA6JqM7BeuHp0inNFPu/izHCksrxC1WA termius-mobile"
 ]
